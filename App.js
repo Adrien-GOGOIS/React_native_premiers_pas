3eaf4a3005ddc788da47cb6103bc6d8179158ef4
Adrien GOGOIS <ad.gogois@gmail.com>
2022-02-28 12:24:15 +0100

    Exo 2 done

--- App.js
+++ App.js
@@ -4,7 +4,13 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>TEST</Text>
+      <View style={styles.whiteBg}>
+        <Text style={(styles.text, styles.big)}>Ceci est du texte</Text>
+        <Text style={(styles.text, styles.centered)}>Et encore du texte</Text>
+        <Text style={(styles.text, styles.bold)}>
+          Bon je crois qu'on a compris...
+        </Text>
+      </View>
     </View>
   );
 }
@@ -12,8 +18,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "rgba(0, 0, 255, 0.5)",
     alignItems: "center",
     justifyContent: "center",
+  },
+
+  whiteBg: {
+    backgroundColor: "white",
+  },
+
+  text: {
+    margin: "80 0",
+  },
+
+  big: {
+    fontSize: 30,
+  },
+
+  centered: {
+    textAlign: "center",
+  },
+
+  bold: {
+    fontWeight: "bold",
   },
 });
